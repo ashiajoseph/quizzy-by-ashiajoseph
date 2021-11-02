089cf3227e11c9ea132e48c19d31994ba861c10c
ashiajoseph <ashiajoseph@gmail.com>
2021-11-02 15:33:55 +0530

    Added email constraints and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
+
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+
+  before_save :to_lowercase
+
+  private
+
+    def to_lowercase
+      email.downcase!
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_074050) do
+ActiveRecord::Schema.define(version: 2021_11_02_093451) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_11_02_074050) do
     t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
