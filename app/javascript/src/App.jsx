2191249b6053338442f1345ff39ec2012728ed0f
ashiajoseph <ashiajoseph@gmail.com>
2021-11-03 17:57:27 +0530

    Set axios header

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,15 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
+import { setAuthHeaders } from "./apis/axios";
 import { initializeLogger } from "./common/logger";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     /*eslint no-undef: "off"*/
     initializeLogger();
+    setAuthHeaders(setLoading);
   }, []);
-  logger.error("---");
+
+  if (loading) return <h2>Loading ...</h2>;
+
   return (
     <Router>
       <Switch>
