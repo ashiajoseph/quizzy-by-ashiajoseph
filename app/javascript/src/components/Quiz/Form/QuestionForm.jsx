9e89ac574b25903e84d484043f56c8951fddc8e4
ashiajoseph <ashiajoseph@gmail.com>
2021-11-08 20:35:38 +0530

    Added form fields and styled it

--- app/javascript/src/components/Quiz/Form/QuestionForm.jsx
+++ app/javascript/src/components/Quiz/Form/QuestionForm.jsx
@@ -1,27 +1,51 @@
-import React, { useState } from "react";
+import React from "react";
 
-//import Button from "../../Button";
 import { Plus, Minus } from "@bigbinary/neeto-icons";
 import { Tooltip } from "@bigbinary/neetoui/v2";
 
+import Button from "../../Button";
 import Input from "../../Input";
 
-const QuestionForm = ({ action = "create", handleSubmit, heading }) => {
-  const [optionList, setOptionList] = useState([{ value: "" }, { value: "" }]);
-
+const QuestionForm = ({
+  action = "create",
+  handleSubmit,
+  heading,
+  setQA,
+  optionList,
+  setOptionList,
+}) => {
   const handleChange = e => {
     const { name, value } = e.target;
-    logger.info(name, value);
-  };
-  const addOption = () => {
-    if (optionList.length < 4) {
-      setOptionList(optList => [...optList, { value: "" }]);
+    if (name.includes("option")) {
+      setOptionList(prev => {
+        return { ...prev, [`${name}`]: value };
+      });
+    } else {
+      const val = name == "answer" ? optionList[value] : value;
+      setQA(prev => {
+        return { ...prev, [name]: val };
+      });
     }
   };
-  const removeOption = index => {
-    setOptionList(optList => optList.filter((item, ind) => ind !== index + 2));
+  const addOption = () => {
+    const len = Object.keys(optionList).length;
+    if (len < 4) {
+      setOptionList(optList => {
+        return { ...optList, [`option${len + 1}`]: "" };
+      });
+    }
   };
-  let hide = optionList.length == 4 ? "hidden" : null;
+  const removeOption = () => {
+    const res = Object.keys(optionList)
+      .slice(0, -1)
+      .reduce((res, opt) => {
+        const value = optionList[opt];
+        return { ...res, [`${opt}`]: value };
+      }, {});
+    setOptionList(res);
+  };
+
+  let hide = Object.keys(optionList).length == 4 ? "hidden" : null;
   return (
     <>
       <div className="flex flex-col items-center justify-center my-10 ">
@@ -33,7 +57,7 @@ const QuestionForm = ({ action = "create", handleSubmit, heading }) => {
             {action == "create" ? "Add question" : "Edit question"}
           </h2>
         </div>
-        <div className="w-10/12	">
+        <div className="w-10/12	my-4">
           <form onSubmit={handleSubmit}>
             <div className="w-3/4 mx-auto">
               <Input
@@ -51,40 +75,41 @@ const QuestionForm = ({ action = "create", handleSubmit, heading }) => {
                 <Input
                   label="Option 1"
                   type="text"
-                  name="option"
+                  name="option1"
                   placeholder=""
                   onChange={handleChange}
                 />
                 <Input
                   label="Option 2"
                   type="text"
-                  name="option"
+                  name="option2"
                   placeholder=""
                   onChange={handleChange}
                 />
               </div>
               <div className="w-2/5 py-5">
-                {optionList.slice(2).map(({ value }, index) => (
-                  <div key={index} className="relative">
-                    <Input
-                      label={`Option ${index + 3}`}
-                      type="text"
-                      name="option"
-                      placeholder=""
-                      value={value}
-                      onChange={handleChange}
-                    />
-                    <Tooltip position="right-end" content="Remove">
-                      <button
-                        type="button"
-                        onClick={() => removeOption(index)}
-                        className="absolute -bottom-1 right-0 bg-red-500 m-1 rounded-full focus:outline-none "
-                      >
-                        <Minus className="neeto-ui-text-white" />
-                      </button>
-                    </Tooltip>
-                  </div>
-                ))}
+                {Object.keys(optionList)
+                  .slice(2)
+                  .map((name, index) => (
+                    <div key={index} className="relative">
+                      <Input
+                        label={`Option ${index + 3}`}
+                        type="text"
+                        name={name}
+                        placeholder=""
+                        onChange={handleChange}
+                      />
+                      <Tooltip position="right-end" content="Remove">
+                        <button
+                          type="button"
+                          onClick={() => removeOption()}
+                          className="absolute -bottom-1 right-0 bg-red-500 m-1 rounded-full focus:outline-none "
+                        >
+                          <Minus className="neeto-ui-text-white" />
+                        </button>
+                      </Tooltip>
+                    </div>
+                  ))}
                 <button
                   type="button"
                   onClick={addOption}
@@ -94,6 +119,31 @@ const QuestionForm = ({ action = "create", handleSubmit, heading }) => {
                   <Plus className="inline" /> Add Option
                 </button>
               </div>
+            </div>
+            <div className="w-1/2 mx-auto py-4">
+              <label
+                className="block text-md font-medium
+                leading-5 text-gray-800"
+              >
+                Correct Answer
+              </label>
+              <select
+                name="answer"
+                className=" block w-full  px-3 py-2 transition duration-150 ease-in-out border border-gray-300 rounded-md bg-gray-800 text-white"
+                onChange={handleChange}
+              >
+                {Object.keys(optionList).map((option, index) => (
+                  <option key={index} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="w-1/3 mx-auto mt-4">
+              <Button
+                type="submit"
+                buttonText={action == "create" ? "Submit" : "Update"}
+              />
             </div>
           </form>
         </div>
--- app/javascript/src/components/Quiz/Question/CreateQuestion.jsx
+++ app/javascript/src/components/Quiz/Question/CreateQuestion.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
+import Logger from "js-logger";
 import { useLocation } from "react-router-dom";
 
 import Container from "../../Container";
 import QuestionForm from "../Form/QuestionForm";
 
 const CreateQuestion = () => {
+  const [qa, setQA] = useState({ question: "", answer: "" });
+  const [optionList, setOptionList] = useState({ option1: "", option2: "" });
   const location = useLocation();
   const heading = location.state;
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    Logger.info(qa);
+    Logger.info(optionList);
+  };
   return (
     <Container>
-      <QuestionForm heading={heading} />
+      <QuestionForm
+        heading={heading}
+        handleSubmit={handleSubmit}
+        optionList={optionList}
+        setOptionList={setOptionList}
+        setQA={setQA}
+      />
     </Container>
   );
 };
