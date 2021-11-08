a5adece1c9ee1fbfeb987f045856ab437793b7e1
ashiajoseph <ashiajoseph@gmail.com>
2021-11-08 11:04:20 +0530

    removed comments

--- app/javascript/src/components/Dashboard.jsx
+++ app/javascript/src/components/Dashboard.jsx
@@ -58,22 +58,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-/*
-return (
-      <Container>
-        <div className="px-10 py-8 mt-4 flex flex-col">
-          <div className="flex justify-end">
-            <AddLink name="Add new quiz" path="/quiz/create" style="" />
-          </div>
-          <Typography
-            lineHeight="normal"
-            style="h1"
-            weight="light"
-            className=" text-center mt-32 py-10 text-gray-600	"
-          >
-            You have not created any quiz
-          </Typography>
-        </div>
-      </Container>
-    );
-*/
