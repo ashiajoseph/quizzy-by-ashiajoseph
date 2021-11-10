f11fe4068a486dd42fed8d17bd9fbf4e0383690d
ashiajoseph <ashiajoseph@gmail.com>
2021-11-10 16:24:02 +0530

    Styled QA list

--- app/javascript/src/components/Quiz/Question/ShowQA.jsx
+++ app/javascript/src/components/Quiz/Question/ShowQA.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 
+import { Delete, Edit } from "@bigbinary/neeto-icons";
+import { Tooltip } from "@bigbinary/neetoui/v2";
+
 import Answer from "./Answer";
 
 const ShowQA = ({ questionList, optionList }) => {
   return (
-    <div className="flex flex-col w-70 mx-auto">
+    <div className="flex flex-col w-3/4 mx-auto">
       {questionList.map(({ question }, index) => (
         <div
           key={index}
-          className="mt-10 border-b-2 border-opacity-75	border-gray-300	"
+          className="mt-6 border-b-2 border-opacity-75	border-gray-300	pb-8"
         >
-          <div className="flex flex-row mb-3 justify-between text-lg">
-            <div className="flex flex-row ">
-              <p className="font-light mx-4 text-lg">Question {index + 1}</p>
-              <p className="font-medium text-lg break-words">{question}</p>
-            </div>
-            <div className="mx-4">Edit Delete</div>
+          <div className="flex justify-end mb-2">
+            <Tooltip position="bottom" content="Edit">
+              <button className="focus:outline-none ml-5 " onClick={() => {}}>
+                <Edit size={32} />
+              </button>
+            </Tooltip>
+            <Tooltip position="bottom" content="Delete">
+              <button className="focus:outline-none ml-5 " onClick={() => {}}>
+                <Delete size={28} className="neeto-ui-text-error" />
+              </button>
+            </Tooltip>
+          </div>
+          <div className="flex flex-row w-full">
+            <h3 className="font-light mx-4 text-lg w-10">
+              Question {index + 1}
+            </h3>
+            <h3 className="font-medium text-lg break-words w-4/5 mr-5">
+              {question}
+            </h3>
           </div>
           <Answer options={optionList[index]} />
         </div>
