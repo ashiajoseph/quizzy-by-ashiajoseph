8e813d1a4f025d1eb44e87419de10f6d5560b01c
ashiajoseph <ashiajoseph@gmail.com>
2021-11-20 10:34:14 +0530

    Added export and export_status action to ReportsController

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,4 +2,26 @@
 
 class ReportsController < ApplicationController
   before_action :authenticate_user_using_x_auth_token, only: %i[export export_status]
+
+  def export
+    job_id = ExportReportWorker.perform_async(@current_user.id)
+    render json: {
+      jid: job_id
+    }
+  end
+
+  def export_status
+    job_id = params[:id]
+    job_status = Sidekiq::Status.get_all(job_id).symbolize_keys
+
+    render json: {
+      status: job_status[:status],
+      percentage: job_status[:pct_complete]
+    }
+  end
+<<<<<<< ours
+=======
+
+  
+>>>>>>> theirs
 end
