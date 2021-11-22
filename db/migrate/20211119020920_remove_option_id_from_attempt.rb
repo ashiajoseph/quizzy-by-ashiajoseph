56a7c749134181b2dbea3dda8e1389a8ed647ea6
ashiajoseph <ashiajoseph@gmail.com>
2021-11-22 11:32:32 +0530

    Dropped database and created again. Removed unnecessary migration files

--- db/migrate/20211119020920_remove_option_id_from_attempt.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveOptionIdFromAttempt < ActiveRecord::Migration[6.1]
-  def change
-    remove_reference :attempts, :option_id_id, index: true
-  end
-end
--- db/migrate/20211119023856_remove_option_id_idfrom_attempt.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveOptionIdIdfromAttempt < ActiveRecord::Migration[6.1]
-  def change
-    remove_index :attempts, column: :option_id_id
-    remove_column :attempts, :option_id_id
-  end
-end
