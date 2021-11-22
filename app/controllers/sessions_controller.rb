52635d591231d12169ff0dd5c337e397fc946cf1
ashiajoseph <ashiajoseph@gmail.com>
2021-11-22 15:40:13 +0530

    Access denied for standard users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,14 @@
 class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: login_params[:email].downcase)
-    unless @user.present? && @user.authenticate(login_params[:password])
+    isAdministrator = @user.present? && (@user.role == "administrator")
+    puts isAdministrator
+    if !@user.present? || !@user.authenticate(login_params[:password])
       render status: :unauthorized, json: { error: t("session.incorrect_credentials") }
+    else
+      unless isAdministrator
+        render status: :unauthorized, json: { error: t("session.access_denied") }
+      end
     end
   end
 
